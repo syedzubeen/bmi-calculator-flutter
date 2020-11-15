b1e48d385776f166887c8c1f7d4f3a7a58321a2e
zubeen <zubeenqadry@gmail.com>
2020-11-15 13:32:39 +0530

    third commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,11 +1,26 @@
+import 'package:bmi_calculator/reusable_dart.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:bmi_calculator/icon_content.dart';
 
+const bottomContainerHeight = 70.0;
+const activeCardColour = Color(0xFF1D1F33);
+const inactiveCardColour = Color(0xFF1A237E);
+const bottomContainerColor = Colors.pinkAccent;
+
+enum Gender{
+  male,
+  female,
+}
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,52 +29,69 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: [
-          Expanded(child: Row(
+          Expanded(
+              child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFF1D1F33),
+              Expanded(
+                child : GestureDetector(
+                  onTap: (){
+                        setState(() {
+                         selectedGender=Gender.male;
+                        });
+                  },
+                child: ReusableCard(
+                  colour: selectedGender==Gender.male?activeCardColour:inactiveCardColour,
+                  cardChild: IconContent(icon: FontAwesomeIcons.mars,label: 'MALE'),
                 ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFF1D1F33),
                 ),
-              ),),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      selectedGender=Gender.female;
+                    });
+                  },
+                  child: ReusableCard(
+                    colour: selectedGender==Gender.female?activeCardColour:inactiveCardColour,
+                    cardChild: IconContent(icon: FontAwesomeIcons.venus,label: 'FEMALE'),
+                  ),
+                ),
+              ),
             ],
           )),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Color(0xFF1D1F33),
+          Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
             ),
-          ),),
-          Expanded(child: Row(
+          ),
+          Expanded(
+              child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFF1D1F33),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
                 ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Color(0xFF1D1F33),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
                 ),
-              ),),
+              ),
             ],
           )),
+          Container(
+            color: bottomContainerColor,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+          ),
         ],
-      )
+      ),
     );
   }
 }
+
+
+
 
